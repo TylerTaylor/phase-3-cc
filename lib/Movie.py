12930d52c6cb1fdfaa93997d6be8c1f2304f3187
Duane Grell <duanegrell@gmail.com>
2023-03-03 12:08:44 -0500

    CC submission

--- lib/Movie.py
+++ lib/Movie.py
@@ -1,13 +1,36 @@
 class Movie:
     
     def __init__(self, title):
-        pass
+        self.title = title
+
+        self.reviews = []
+        self.reviewers = []
 
     # title property goes here!
+    @property
+    def title(self):
+        return self._title
 
+    @title.setter
+    def title(self, title):
+        if type(title) == str and len(title) > 0:
+            self._title = title
+        else:
+            raise Exception ("need a string please")
+
+    @classmethod
     def average_rating(self):
-        pass
+        return sum ([review.rating for review in self.reviews]) / len(self.reviews)
 
     @classmethod
     def highest_rated(cls):
-        pass
+        max_rating = max (review.rating for review in cls.reviews)
+        if cls.review.rating == max_rating: 
+            max_movie = cls.title
+
+        return max ()
+
+
+lion_king = Movie("Lion King")
+
+print(lion_king.title)
--- lib/Viewer.py
+++ lib/Viewer.py
@@ -1,12 +1,52 @@
 class Viewer:
     
+    usernames = []
+
     def __init__(self, username):
-        pass
+        if (username not in Viewer.usernames):
+            self.username = username
+        else:
+            raise Exception ("Username already exists")
+
+        Viewer.usernames.append(self)
+        
+        self.reviewed_movies = []
+        self.reviews = []
+
 
     # username property goes here!
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        if (5 < len(username) < 17) and (username not in Viewer.usernames):
+           self._username = username
+        else:
+            raise Exception ("Ty Again please")
+        
+    # CHECKING FOR REVIEWED MOVIES
 
     def reviewed_movie(self, movie):
-        pass
+        if movie in self.reviewed_movies:
+            return True
+        else:
+            return False
+
+    # RATING NEW MOVIE
 
     def rate_movie(self, movie, rating):
-        pass
+        from lib.Review import Review
+        Review(self, movie, rating)
+
+        if movie in self.reviewed_movies:
+            Review.rating = rating
+        else:
+            Review(self, movie, rating)
+        
+        
+
+duane = Viewer("duanegrell")
+
+print (duane.reviewed_movie("Up"))
